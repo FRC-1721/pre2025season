18457cc0c8abc9044868150e7b13257a5e7851ac
dublUayaychtee <mail@dublu.net>
2025-01-06 16:01:04 -0500

    that's not what it's called

--- robot.py
+++ robot.py
@@ -5,10 +5,10 @@ import rev
 class Robot(magicbot.MagicRobot):
     def robotInit(self):
         self.joystick = wpilib.Joystick(0)
-        self.left = rev.SparkMax(1, rev.SparkLevel.motorType.kBrushless)
-        self.left2 = rev.SparkMax(3, rev.SparkLevel.motorType.kBrushless)
-        self.right = rev.SparkMax(2, rev.SparkLevel.motorType.kBrushless)
-        self.right2 = rev.SparkMax(4, rev.SparkLevel.motorType.kBrushless)
+        self.left = rev.SparkMax(1, rev.SparkLowLevel.motorType.kBrushless)
+        self.left2 = rev.SparkMax(3, rev.SparkLowLevel.motorType.kBrushless)
+        self.right = rev.SparkMax(2, rev.SparkLowLevel.motorType.kBrushless)
+        self.right2 = rev.SparkMax(4, rev.SparkLowLevel.motorType.kBrushless)
 
         self.left2.follow(self.left)
         self.right2.follow(self.right)
